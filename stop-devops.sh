a76043caa46ca2b00f730555280fddcae2357345
metavige <metavige@gmail.com>
2021-08-10 08:23:17 +0800

    feat: replace traefik labels
    - 移除一些不需要在每個 compose 內設定的 traefik

---
+++ stop-devops.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DOT_ENV="${PWD}/.env"
+COMPOSE_FILE="$1/docker-compose.yml"
+
+if [ -d $1 ]
+then
+  docker compose --env-file=$DOT_ENV -f $COMPOSE_FILE stop
+else
+  echo "$1 not existed!"
+fi
